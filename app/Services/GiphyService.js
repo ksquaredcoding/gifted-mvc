879772c2d4f397f15ba8f75828b88a5476050ce4
Kevin Kelley <ksquaredcoding@gmail.com>
2022-09-15 17:13:35 -0600

    needed params and some curly bois

--- app/Services/GiphyService.js
+++ app/Services/GiphyService.js
@@ -6,10 +6,12 @@ import { GiphyServer, SandboxServer } from "./AxiosService.js"
 class GiphyService {
   async search(query) {
     const res = await GiphyServer.get('search', {
-      key: 'XhhdJHA8U0uQIiQzXv9OQ6FiZjTzZSTJ',
-      rating: 'pg',
-      limit: 10,
-      q: query
+      params: {
+        api_key: 'XhhdJHA8U0uQIiQzXv9OQ6FiZjTzZSTJ',
+        rating: 'pg',
+        limit: 10,
+        q: query
+      }
     })
     appState.giphyGifs = res.data.map(g => new GiphyGif(res.data))
   }
